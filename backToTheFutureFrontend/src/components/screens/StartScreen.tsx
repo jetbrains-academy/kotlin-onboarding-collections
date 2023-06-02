62661fce62cb10bfc800bd05c4e4e1c28e714717
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-06-02 10:18:18 +0200

    Add base back to the future

---
+++ backToTheFutureFrontend/src/components/screens/StartScreen.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/logo.svg";
+import {GameState} from "../GameScreen";
+
+type StartScreenProps = {
+    gameStateSetter: (gs: GameState) => void
+}
+
+export default function StartScreen({gameStateSetter}: StartScreenProps) {
+    return (
+        <div className="App-main-container">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p className="App-big-name font-link-bold">Back To The Future</p>
+            <p className="App-small-name font-link-base">by Kotlin Course</p>
+            <div className="App-buttons-container App-display-flex">
+                <button className="App-button-base App-button-start"></button>
+            </div>
+        </div>
+    );
+}
