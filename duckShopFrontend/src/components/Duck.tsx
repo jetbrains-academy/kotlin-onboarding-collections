05dd9e054c2243bcdeb6ac176952b3b8814c982b
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-08-11 19:20:02 +0200

    Finish with the duck shop

--- duckShopFrontend/src/components/Duck.tsx
+++ duckShopFrontend/src/components/Duck.tsx
@@ -1,15 +1,24 @@
 import {DuckInfo} from "../models/DuckInfo";
+import {PARTITION_FUNCTION} from "./screens/MainActionsScreen";
 
 type DuckProps = {
-    duck: DuckInfo
+    duck: DuckInfo,
+    pressedFunction: String
 }
 
-export default function Duck({duck}: DuckProps) {
+export default function Duck({duck, pressedFunction}: DuckProps) {
     let baseDucksClasses = "App-duck-image "
     let baseLabelClasses = "App-duck-container-label "
 
+    function toAddBest() {
+        if (pressedFunction != PARTITION_FUNCTION) {
+            return false
+        }
+        return duck.hasKotlinAttribute
+    }
+
     return(
-        <div className="App-duck-container">
+        <div className={"App-duck-container" + (toAddBest() ? " App-duck-container-best" : "")}>
             <div className={duck.imageClass == null ? baseDucksClasses : baseDucksClasses + duck.imageClass}></div>
             <div className={duck.description == null ? baseLabelClasses + "App-display-none" : baseLabelClasses}>{duck.description}</div>
         </div>
--- duckShopServer/duckShopServerIntroduction/src/main/kotlin/org/jetbrains/kotlin/course/duck/shop/mode/GameModeResource.kt
+++ duckShopServer/duckShopServerIntroduction/src/main/kotlin/org/jetbrains/kotlin/course/duck/shop/mode/GameModeResource.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.kotlin.course.duck.shop.mode
 
 import duck.shop.JsDuck
+import org.jetbrains.kotlin.course.duck.shop.utils.GameMode
+import org.jetbrains.kotlin.course.duck.shop.utils.toJsDuck
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,15 +14,15 @@ class CardResource(val service: GameModeService) {
     @CrossOrigin
     @GetMapping("/list")
     fun generateListOfCharacters(): List<JsDuck> =
-        service.generateListOfDucks().map { JsDuck(it.customName ?: it.name, hasKotlinAttribute = it.hasKotlinAttribute) }
+        service.generateListOfDucks().map { it.toJsDuck(GameMode.List) }
 
     @CrossOrigin
     @GetMapping("/set")
     fun generateSetOfCharacters(): List<JsDuck> = service.generateSetOfDucks()
-        .map { JsDuck(it.customName ?: it.name, hasKotlinAttribute = it.hasKotlinAttribute) }
+        .map { it.toJsDuck(GameMode.Set) }
 
     @CrossOrigin
     @GetMapping("/map")
     fun generateMapOfCharacters(): List<JsDuck> = service.generateMapOfDucks()
-        .map { JsDuck(it.key.name, it.value, hasKotlinAttribute = it.key.hasKotlinAttribute) }
+        .map { it.key.toJsDuck(GameMode.Map) }
 }
