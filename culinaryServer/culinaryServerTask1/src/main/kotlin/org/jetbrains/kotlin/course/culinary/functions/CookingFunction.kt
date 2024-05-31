14aa5c99178be086cdab5e2db38901bec13eac98
Iaroslav Postovalov <iaroslav.postovalov@jetbrains.com>
2024-05-31 13:33:05 +0200

    Kitchen clearing, add endpoints with ready tasks

--- culinaryServer/culinaryServerTask1/src/main/kotlin/org/jetbrains/kotlin/course/culinary/functions/CookingFunction.kt
+++ culinaryServer/culinaryServerTask1/src/main/kotlin/org/jetbrains/kotlin/course/culinary/functions/CookingFunction.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.kotlin.course.culinary.functions
 
 import culinary.JsAction
+import org.jetbrains.kotlin.course.culinary.*
 import org.jetbrains.kotlin.course.culinary.actions
 import org.springframework.http.StreamingHttpOutputMessage
 import org.springframework.web.bind.annotation.*
@@ -9,9 +10,68 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/functions/")
 class CookingFunction(val service: CookingService) {
     @CrossOrigin
-    @PostMapping("/cooking")
+    @GetMapping("/cooking")
     fun recipe(@RequestBody body: StreamingHttpOutputMessage.Body): List<JsAction> {
         service.performCooking()
+        clearKitchen()
+        return actions
+    }
+
+    @CrossOrigin
+    @GetMapping("/test-task1")
+    fun task1(@RequestBody body: StreamingHttpOutputMessage.Body): List<JsAction> {
+        val vegetables = List(3) { fridge.getVegetable(what = VegetableType.Tomato) }
+        vegetables
+            .onEach { counter.put(it) }
+            .map { counter.cut(it) }
+            .forEach { pot.put(counter.take(it)) }
+        pot.simmer()
+        clearKitchen()
+        return actions
+    }
+
+    @CrossOrigin
+    @GetMapping("/test-task2")
+    fun task2(@RequestBody body: StreamingHttpOutputMessage.Body): List<JsAction> {
+        val spices = generateSequence { SpiceType.entries.random() }
+        spices
+            .map { shelf.getSpice(it) }
+            .map { pot.put(it) }
+            .takeWhile { !pot.doesTastePerfect() }
+            .toList() // terminate
+        pot.simmer()
+        clearKitchen()
+        return actions
+    }
+
+    @CrossOrigin
+    @GetMapping("/test-task3")
+    fun task3(@RequestBody body: StreamingHttpOutputMessage.Body): List<JsAction> {
+        fridge.getAllVegetables()
+            .map { counter.put(it) }
+            .filter { counter.checkFresh(it) }
+            .map { counter.cut(it) }
+            .take(5)
+            .map { counter.take(it) }
+            .groupBy { it.type }
+            .forEach { (type, cuts) -> saladBowl.add(type, cuts) }
+        saladBowl.mix()
+        clearKitchen()
+        return actions
+    }
+
+    @CrossOrigin
+    @GetMapping("/test-task4")
+    fun task4(@RequestBody body: StreamingHttpOutputMessage.Body): List<JsAction> {
+        val fruits = listOf(FruitType.Citrus, FruitType.Berry)
+        fruits.map { type -> fridge.getBasketOf(type) }
+            .onEach { basket -> counter.put(basket) }
+            .flatMap { basket -> List(basket.capacity) { counter.takeFromBasket(basket) } }
+            .distinctBy { it.type }
+            .sortedBy { it.type.sugarContent }
+            .forEach { blender.add(it) }
+        blender.blend()
+        clearKitchen()
         return actions
     }
 }
--- culinaryServer/culinaryServerTask1/src/main/kotlin/org/jetbrains/kotlin/course/culinary/functions/CookingService.kt
+++ culinaryServer/culinaryServerTask1/src/main/kotlin/org/jetbrains/kotlin/course/culinary/functions/CookingService.kt
@@ -1,59 +1,10 @@
 package org.jetbrains.kotlin.course.culinary.functions
 
-import org.jetbrains.kotlin.course.culinary.*
 import org.springframework.stereotype.Service
 
 @Service
 class CookingService {
     fun performCooking() {
-        fun task1() {
-            val vegetables = List(3) { fridge.getVegetable(what = VegetableType.Tomato) }
-            vegetables
-                .onEach { counter.put(it) }
-                .map { counter.cut(it) }
-                .forEach { pot.put(counter.take(it)) }
-
-            pot.simmer()
-        }
-
-        fun task2() {
-            val spices = generateSequence { SpiceType.entries.random() }
-            spices
-                .map { shelf.getSpice(it) }
-                .map { pot.put(it) }
-                .takeWhile { !pot.doesTastePerfect() }
-                .toList() // terminate
-
-            pot.simmer()
-        }
-
-        fun task3() {
-            fridge.getAllVegetables()
-                .map { counter.put(it) }
-                .filter { counter.checkFresh(it) }
-                .map { counter.cut(it) }
-                .take(5)
-                .map { counter.take(it) }
-                .groupBy { it.type }
-                .forEach { (type, cuts) -> saladBowl.add(type, cuts) }
-
-            saladBowl.mix()
-        }
-
-        fun task4() {
-            val fruits = listOf(FruitType.Citrus, FruitType.Berry)
-
-            fruits.map { type -> fridge.getBasketOf(type) }
-                .onEach { basket -> counter.put(basket) }
-                .flatMap { basket -> List(basket.capacity) { counter.takeFromBasket(basket) } }
-                .distinctBy { it.type }
-                .sortedBy { it.type.sugarContent }
-                .forEach { blender.add(it) }
-
-            blender.blend()
-        }
-
-        task4()
-        println(actions.joinToString("\n"))
+        TODO()
     }
 }
