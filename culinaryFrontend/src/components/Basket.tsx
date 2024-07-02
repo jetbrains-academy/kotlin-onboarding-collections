092670593ba4cc78c523911d614c317fba2610b7
Konstantin Chaika <konstantin.chaika@jetbrains.com>
2024-07-02 20:32:08 +0400

    Add multiple fruit and vegetables entities support

--- culinaryFrontend/src/components/Basket.tsx
+++ culinaryFrontend/src/components/Basket.tsx
@@ -1,11 +1,28 @@
 type BasketProps = {
-    type: string,
-    visible: boolean
+    basketOptions: {
+        type: string,
+        visible: boolean,
+        count: number,
+    }
 }
 
-export default function Basket({type, visible}: BasketProps) {
-    let BasketClass = "App-" + type + "_basket"
+export default function Basket({basketOptions:{type, visible, count}}: BasketProps) {
+    let BasketClass = "App-basket "
+    let CurrentBasketClass = `App-basket-${type}-`
     return(
-        <div className={BasketClass + (visible ? "" : " App-invisible")}></div>
+        <div className={BasketClass + (visible ? "" : " App-invisible")}>
+            <div className={"App-basket App-basket-back"}
+            ></div>
+            {count > 0 && (
+                <>
+                    {Array.from({length: count}).map((_, i) => (
+                        <div key={i} className={"App-basket App-basket-item " + CurrentBasketClass + (count - i)}
+                        ></div>
+                    ))}
+                </>
+            )}
+            <div className={"App-basket App-basket-front"}
+            ></div>
+        </div>
     )
 }
